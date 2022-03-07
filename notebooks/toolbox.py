ba612d498ba6a948582f79ab061bfa4fb490d255
Claudio <c.m.pierard@uu.nl>
2022-03-07 16:47:35 +0100

    Find the particles that are stuck on the seafloor. Wrote a function to mask them. Don't have a solution to implement in the model

---
+++ notebooks/toolbox.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def stuck_particles_mask(dataset):
+    """
+    Function that masks the particles that are stuck on the seafloor from xarray
+    datasets.
+    
+    dataset: and xarray dataset.
+    """
+    time = dataset.dims['obs']
+    diff_lon = np.roll(dataset['lon'], axis=1, shift=1) - dataset['lon']
+    diff_lat = np.roll(dataset['lat'], axis=1, shift=1) - dataset['lat']
+    diff_z = np.roll(dataset['z'], axis=1, shift=1) - dataset['z']
+    
+    mask = (diff_lon + diff_lat + diff_z)
+    mask = ~(mask == 0)
+    
+    return mask
